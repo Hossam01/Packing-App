8a07752308ec1fc4af11ad09bfc9f9b2e1860e31
Mustafa Elsayed <m01065286596>
2020-09-21 02:17:47 +0200

    Get order data

---
+++ app/src/main/java/com/example/packingapp/GetOrderDatactivity.java
@@ -0,0 +1,14 @@
+package com.example.packingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetOrderDatactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_order_data);
+    }
+}
