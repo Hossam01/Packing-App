936571e0372a205712191d0a4db6905812b83d7f
Mustafa Elsayed <m01065286596>
2020-09-28 15:53:25 +0200

    Logo 28092020_V2

--- app/src/main/java/com/example/packingapp/Retrofit/ApiClient.java
+++ app/src/main/java/com/example/packingapp/Retrofit/ApiClient.java
@@ -5,8 +5,8 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
-    public static final String BASE_URL = "http://192.168.1.64:80/";
-
+   // public static final String BASE_URL = "http://192.168.1.64:80/";
+    public static final String BASE_URL = "http://192.168.1.44:80/";
     public Retrofit build() {
         final Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
