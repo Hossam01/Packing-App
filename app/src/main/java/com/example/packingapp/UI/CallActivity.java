d0cc64dce8e6e9e06f0b01d5a88945e9bcc306c2
Hossam Abdelnaby <analizer61@gmail.com>
2020-10-21 14:27:26 +0200

    Manage Driver,Vehicle,Driver

---
+++ app/src/main/java/com/example/packingapp/UI/CallActivity.java
@@ -0,0 +1,71 @@
+package com.example.packingapp.UI;
+
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.packingapp.R;
+
+public class CallActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call);
+
+        TextView textView = findViewById(R.id.distance);
+        final LocationListener mLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+                textView.setText(String.valueOf(getDistance(location.getLatitude(), location.getLongitude(), 30.0281722, 31.01875)));
+            }
+        };
+
+
+        Button call = findViewById(R.id.call);
+        call.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                Intent intent = getPackageManager().getLaunchIntentForPackage("com.zoiper.android.noinapp.app");
+                if (intent != null) {
+                    // We found the activity now start the activity
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    intent.setData(Uri.parse("tel:" + "01286751382"));
+//                    intent.putExtra("KeyName","01286751382");
+
+                    intent.setAction(Intent.ACTION_SEND);
+                    intent.putExtra(Intent.EXTRA_TEXT, "01286751382");
+                    intent.setType("text/plain");
+                    Intent shareIntent = Intent.createChooser(intent, null);
+
+                    startActivity(shareIntent);
+                } else {
+                    // Bring user to the market or let them choose an app?
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(Uri.parse("market://details?id=" + "com.zoiper.android.noinapp.app"));
+                    startActivity(intent);
+                }
+
+            }
+        });
+    }
+
+    private float getDistance(double lat1, double lon1, double lat2, double lon2) {
+        float[] distance = new float[2];
+        Location.distanceBetween(lat1, lon1, lat2, lon2, distance);
+        return distance[0] + distance[1];
+    }
+
+
+}
---
+++ app/src/main/java/com/example/packingapp/UI/INOUTActivity.java
@@ -0,0 +1,84 @@
+package com.example.packingapp.UI;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.packingapp.R;
+import com.example.packingapp.Retrofit.APIRetrofit;
+import com.example.packingapp.Retrofit.ApiClient;
+import com.example.packingapp.model.Message;
+
+import java.util.HashMap;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class INOUTActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i_n_o_u_t);
+
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put("driverId", "2");
+        map.put("status", "1");
+
+        Button in = findViewById(R.id.checkin);
+        in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData(map);
+            }
+        });
+
+        Button out = findViewById(R.id.checkout);
+        out.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData(map);
+            }
+        });
+
+
+    }
+
+    public void sendData(HashMap<String, String> map) {
+        ApiClient apiClient = new ApiClient();
+        APIRetrofit client = apiClient.build().create(APIRetrofit.class);
+        Observable<Message> call = client.createDriver(map)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+        Observer<Message> observer = new Observer<Message>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Message message) {
+                Toast.makeText(getApplicationContext(), message.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        };
+        call.subscribe(observer);
+    }
+}
