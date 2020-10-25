cf33336f075303fac0413772820f5a8a1a0dbed0
Mustafa Elsayed <m01065286596>
2020-10-25 15:04:56 +0200

    call added

--- app/src/main/java/com/example/packingapp/UI/CallActivity.java
+++ app/src/main/java/com/example/packingapp/UI/CallActivity.java
@@ -1,6 +1,8 @@
 package com.example.packingapp.UI;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.net.Uri;
@@ -12,10 +14,13 @@ import android.widget.TextView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.packingapp.R;
 
 public class CallActivity extends AppCompatActivity {
+
+    private static final int REQUEST_PHONE_CALL = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,36 +31,43 @@ public class CallActivity extends AppCompatActivity {
         final LocationListener mLocationListener = new LocationListener() {
             @Override
             public void onLocationChanged(final Location location) {
-                textView.setText(String.valueOf(getDistance(location.getLatitude(), location.getLongitude(), 30.0281722, 31.01875)));
+                textView.setText(String.valueOf(getDistance(location.getLatitude(), location.getLongitude(),
+                        30.0281722, 31.01875)));
             }
         };
 
+        PermissionForCall();
 
         Button call = findViewById(R.id.call);
         call.setOnClickListener(new View.OnClickListener() {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View v) {
-                Intent intent = getPackageManager().getLaunchIntentForPackage("com.zoiper.android.noinapp.app");
-                if (intent != null) {
-                    // We found the activity now start the activity
+                Intent intent = new Intent(Intent.ACTION_CALL);
+
+                intent.setData(Uri.parse("tel:01065551910"));
+                startActivity(intent);
+
+//                Intent intent = getPackageManager().getLaunchIntentForPackage("com.zoiper.android.noinapp.app");
+//                if (intent != null) {
+//                    // We found the activity now start the activity
+////                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+////                    intent.setData(Uri.parse("tel:" + "01286751382"));
+////                    intent.putExtra("KeyName","01286751382");
+//
+//                    intent.setAction(Intent.ACTION_SEND);
+//                    intent.putExtra(Intent.EXTRA_TEXT, "01286751382");
+//                    intent.setType("text/plain");
+//                    Intent shareIntent = Intent.createChooser(intent, null);
+//
+//                    startActivity(shareIntent);
+//                } else {
+//                    // Bring user to the market or let them choose an app?
+//                    intent = new Intent(Intent.ACTION_VIEW);
 //                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    intent.setData(Uri.parse("tel:" + "01286751382"));
-//                    intent.putExtra("KeyName","01286751382");
-
-                    intent.setAction(Intent.ACTION_SEND);
-                    intent.putExtra(Intent.EXTRA_TEXT, "01286751382");
-                    intent.setType("text/plain");
-                    Intent shareIntent = Intent.createChooser(intent, null);
-
-                    startActivity(shareIntent);
-                } else {
-                    // Bring user to the market or let them choose an app?
-                    intent = new Intent(Intent.ACTION_VIEW);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setData(Uri.parse("market://details?id=" + "com.zoiper.android.noinapp.app"));
-                    startActivity(intent);
-                }
+//                    intent.setData(Uri.parse("market://details?id=" + "com.zoiper.android.noinapp.app"));
+//                    startActivity(intent);
+//                }
 
             }
         });
@@ -67,5 +79,9 @@ public class CallActivity extends AppCompatActivity {
         return distance[0] + distance[1];
     }
 
-
+    public void PermissionForCall(){
+        if (ActivityCompat.checkSelfPermission(CallActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(CallActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+        }
+    }
 }
